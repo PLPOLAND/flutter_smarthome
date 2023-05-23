70025607f14fb17b1e871b0aafb9ca82047a239b
Marek Pałdyna <marekpaldyna@wp.pl>
2023-05-23 14:01:05 +0200

    logout possition in menu

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_smarthome/screens/rooms_page.dart';
+import 'package:provider/provider.dart';
+
+import '../models/bloc/auth/auth_bloc.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({super.key});
@@ -13,33 +16,48 @@ class MainDrawer extends StatelessWidget {
         title: const Text('Menu'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Homepage'),
-            onTap: () => Navigator.of(context).pushReplacementNamed('/'),
+          Column(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Homepage'),
+                onTap: () => Navigator.of(context).pushReplacementNamed('/'),
+              ),
+              ListTile(
+                leading: const Icon(Icons.room_preferences),
+                title: const Text('Rooms'),
+                onTap: () => Navigator.of(context)
+                    .pushReplacementNamed(RoomsPage.routeName),
+              ),
+              ListTile(
+                leading: const Icon(Icons.storage),
+                title: const Text('Devices'),
+                onTap: () =>
+                    Navigator.of(context).pushReplacementNamed('/devices'),
+              ),
+              ListTile(
+                leading: const Icon(Icons.sensors),
+                title: const Text('Sensors'),
+                onTap: () =>
+                    Navigator.of(context).pushReplacementNamed('/sensors'),
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Settings'),
+                onTap: () =>
+                    Navigator.of(context).pushReplacementNamed('/settings'),
+              ),
+            ],
           ),
           ListTile(
-            leading: const Icon(Icons.room_preferences),
-            title: const Text('Rooms'),
-            onTap: () =>
-                Navigator.of(context).pushReplacementNamed(RoomsPage.routeName),
-          ),
-          ListTile(
-            leading: const Icon(Icons.storage),
-            title: const Text('Devices'),
-            onTap: () => Navigator.of(context).pushReplacementNamed('/devices'),
-          ),
-          ListTile(
-            leading: const Icon(Icons.sensors),
-            title: const Text('Sensors'),
-            onTap: () => Navigator.of(context).pushReplacementNamed('/sensors'),
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Settings'),
-            onTap: () =>
-                Navigator.of(context).pushReplacementNamed('/settings'),
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              context.read<AuthBloc>().add(LogOut());
+              Navigator.of(context).pushReplacementNamed('/');
+            },
           ),
         ],
       ),
